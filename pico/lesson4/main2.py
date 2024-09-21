5dad5834bf7be9d10d6f9136bad7f00cc1dc7520
Clovis7607 <fjig5116@gmail.com>
2024-09-21 16:09:32 +0800

    led button

--- pico/lesson4/main2.py
+++ pico/lesson4/main2.py
@@ -1,4 +1,15 @@
 from machine import Timer
 
-tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
-tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+#tim = Timer(period=5000, mode=Timer.ONE_SHOT, callback=lambda t:print(1))
+#tim.init(period=1000, mode=Timer.PERIODIC, callback=lambda t:print(2))
+
+count = 0
+
+def myCallback(t: Timer):
+    global count
+    count += 1
+    print(f"目前myCallback被執行:{count}次")
+    if count >= 10:
+        t.deinit()
+
+led_timer = Timer(period = 1000, mode = Timer.PERIODIC, callback = myCallback)
